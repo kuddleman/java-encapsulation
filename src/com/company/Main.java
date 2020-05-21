cb8c5d1ce91a821e4e89425e96d803704e48c8fe
Kuddleman <edl1155@gmail.com>
2020-05-20 18:10:02 -0700

    set up player class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	  Player player = new Player();
+	  player.name = "Tim";
+	  player.health = 20;
+	  player.weapon = "Sword";
+
+	  int damage = 10;
+	  player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
     }
 }
